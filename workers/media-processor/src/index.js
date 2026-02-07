590e0f7d2ab6d903672b223451832d543f17e897
royk9635 <royk9635@gmail.com>
2026-02-07 22:47:12 +0530

    docs: S3 upload fix guide

--- workers/media-processor/src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-console.log('Media Processor Worker started');
-console.log('Listening for media processing jobs...');
-
-// Placeholder for media processing logic
-setInterval(() => {
-    console.log('Media processor heartbeat...');
-}, 60000);
